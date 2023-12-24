52cdd48141ff181c6bc5c9accc8459f641881366
Đỗ Duy Hưng <54223238+doduyhung1292@users.noreply.github.com>
2023-12-24 15:34:04 +0700

    spark

--- spark/coin_trade/app.py
+++ spark/coin_trade/app.py
@@ -1,11 +1,13 @@
 import sys
+import os
 from analyzer import CoinTradeDataAnalyzer
 
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 def run_service(start_date, start_file):
     analyzer = CoinTradeDataAnalyzer(start_date, start_file)
     analyzer.run()
-
 
 if len(sys.argv) > 2:
     run_service(sys.argv[1], sys.argv[2])
